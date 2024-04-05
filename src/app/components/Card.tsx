335944cc73888b56aa563b2d88db4af913c7aa17
berghach <berghach.chemseddine@gmail.com>
2024-04-05 13:48:50 +0000

    restart the project

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddCard from './addCard'
+
+const Card = () => {
+  return (
+    <div>
+        <AddCard />
+    </div>
+  )
+}
+
+export default Card
---
+++ src/app/components/addCard.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const AddCard = () => {
+  return (
+    <div>
+        <button onClick={() => console.log('add card')}>add card</button>
+    </div>
+  )
+}
+
+export default AddCard
