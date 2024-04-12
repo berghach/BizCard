b744cf6df9967292762e8acfe5c698e73fcccf1e
berghach <berghach.chemseddine@gmail.com>
2024-04-12 22:37:41 +0000

    commit

--- src/app/api/auth/[...nextauth]/options.tsx
+++ src/app/api/auth/[...nextauth]/options.tsx
@@ -1,4 +1,22 @@
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import Email from "next-auth/providers/email";
 
-// export const options;
+export const options: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "Email" },
+                password: {  label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                if (!credentials?.email || !credentials?.password) {
+                    return null;
+                }else{
+                    return req.body;
+                }
+            }
+        })
+    ],
+}
